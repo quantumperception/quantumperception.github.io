0359a82cdf1941e8194bbd86019092dc8b0c4277
Jose Hernandez <jmhernandez2@uc.cl>
2022-01-27 12:47:08 -0300

    initial commit

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+import ligma from './menuItems.mjs';
+
+console.log(ligma);
+
+let div = document.createElement('div');
+let newContent = document.createTextNode("OLA K ASE");
+document.body.append(div);
+div.append(newContent);
---
+++ static/js/menuItems.js
@@ -0,0 +1,45 @@
+const menuItems = [
+    {
+        Id: 1,
+        titulo: 'Inicio',
+        href: 'home.php'
+    },
+    {
+        Id: 7,
+        titulo: 'Contacto',
+        href: 'contact.php'
+    },
+    {
+        Id: 6,
+        titulo: 'Empresa',
+        href: 'about.php'
+    },
+    {
+        Id: 2,
+        titulo: 'Laboratorio',
+        href: 'laboratory.php'
+    },
+    {
+        Id: 4,
+        titulo: 'Alimentos',
+        href: 'foods.php',
+        subitem: []
+    },
+    {
+        Id: 5,
+        titulo: 'Diagnóstico Clínico',
+        href: 'clinical-diagnostic.php'
+    },
+    {
+        Id: 2,
+        titulo: 'Productos',
+        href: 'products.php'
+    },
+    {
+        Id: 3,
+        titulo: 'Software',
+        href: 'software.php'
+    }
+]
+
+export default menuItems;
